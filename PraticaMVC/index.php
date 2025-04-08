fdf27423d53215aa200c8f5b6242c9ed8bbb9d60
CC230286 <efraincortez777@gmail.com>
2025-04-08 15:21:49 -0600

    Update index.php

--- PraticaMVC/index.php
+++ PraticaMVC/index.php
@@ -7,14 +7,11 @@ const PATH = '/lis2025/Pratica7';
 $url = $_SERVER['REQUEST_URI'];
 $slices = explode('/', $url);
 
-// Debug: Verifica la estructura de $slices
-// echo "<pre>"; print_r($slices); echo "</pre>"; 
-
 $controller = empty($slices[3]) ? "IndexController" : $slices[3] . "Controller";
 $method = empty($slices[4]) ? "index" : $slices[4];
 $params = empty($slices[5]) ? [] : array_slice($slices, 5);
 
-// Verifica si la clase existe antes de instanciar
+
 if (!class_exists($controller)) {
     die("Controlador no encontrado: $controller");
 }
